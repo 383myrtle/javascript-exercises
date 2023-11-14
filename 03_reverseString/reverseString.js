00c37f0d76a574a633f81a06dc1a915c37f32673
383myrtle <89387525+383myrtle@users.noreply.github.com>
2023-11-14 14:55:09 +0200

    Complete reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let out = ''
+    for (let i = string.length; i > 0; i--){
+        out = out.concat(string.slice(i-1,i))
+    }
+    return out
 };
 
 // Do not edit below this line
