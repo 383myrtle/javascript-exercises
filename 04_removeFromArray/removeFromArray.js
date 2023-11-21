744ebeedf7ef8e57fc607aae5801db62b540c0e3
383myrtle <89387525+383myrtle@users.noreply.github.com>
2023-11-21 12:16:20 +0200

    Complete exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, element) {
+    for (let n = 0; n < arr.length; n++){
+        for (let i = 1; i < arguments.length; i++){
+            if (arr[n] === arguments[i]){
+                arr.splice(n,1)
+                n--
+            }
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
